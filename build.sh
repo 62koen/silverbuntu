4ed227c1105b532e757b7136d58a448e3572ea0c
Koen <7tcxqcoo@addy.io>
2024-10-17 18:24:51 +0200

    added some packages to install

--- build.sh
+++ build.sh
@@ -13,7 +13,20 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install gnome-shell-theme-yaru
+rpm-ostree install \
+https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+gnome-shell-theme-yaru \
+gnome-shell-extension-appindicator \
+gnome-shell-extension-no-overview \
+gnome-shell-extension-dash-to-dock \
+gnome-shell-extension-user-theme \
+gnome-shell-extension-drive-menu \
+make \
+opendyslexic-fonts \
+samba
+
+flatpak install com.mattjakeman.ExtensionManager
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
